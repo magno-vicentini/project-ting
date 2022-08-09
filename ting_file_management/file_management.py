aa05aec0d05fdc3ebf215b70d763cd7cae1ecd00
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-08-09 15:25:12 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, mode="r") as data:
+            list_data = data.read().split('\n')
+            return list_data
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
