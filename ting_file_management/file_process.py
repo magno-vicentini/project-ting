a80a84f55653e91dd2e60e83f74f0372cc10eef0
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-08-09 15:38:57 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    file_data = txt_importer(path_file)
+    process_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_data),
+        "linhas_do_arquivo": file_data,
+    }
+    sys.stdout.write(str(process_data))
+    instance.enqueue(process_data)
 
 
 def remove(instance):
