ff37ea75fa7151d567ab13f1583f8c3a61cb8957
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-08-09 14:51:19 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.line = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.line)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.line.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.line.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self.line):
+            raise IndexError
+        return self.line[index]
