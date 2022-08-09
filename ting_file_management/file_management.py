67f6feedf952dac20a65881f7ff952204faa388b
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-08-09 15:27:23 -0300

    correções

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,9 +5,9 @@ def txt_importer(path_file):
     if not path_file.endswith('txt'):
         return sys.stderr.write('Formato inválido\n')
     try:
-        with open(path_file, mode="r") as data:
-            list_data = data.read().split('\n')
-            return list_data
+        with open(path_file, mode="r") as news:
+            news_list = news.read().split('\n')
+            return news_list
     except FileNotFoundError:
         return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
 
